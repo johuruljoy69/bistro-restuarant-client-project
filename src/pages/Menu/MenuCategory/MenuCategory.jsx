4861f7c43f0626cca5a3402685fdbecd9b242825
johirjoy <joy.sd69@gmail.com>
2023-06-25 23:58:13 +0600

    Menucategory create and setup

---
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import Cover from "../../Shared/Cover/Cover";
+import MenuItems from "../../Shared/MenuItems/MenuItems";
+
+
+const MenuCategory = ({ items, title, coverImg }) => {
+
+    return (
+        <div className="pt-16">
+            {title && <Cover img={coverImg} title={title}></Cover>}
+            <div className="grid md:grid-cols-2 gap-10 mt-16 mb-16">
+                {
+                    items.map(item => <MenuItems
+                        key={item._id}
+                        item={item}
+                    ></MenuItems>)
+                }
+            </div>
+            <div className="text-center">
+                <Link to={`/order/${title}`}><button className="btn btn-outline border-0 border-b-4 mt-5">Order Your Favorite Food</button></Link>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCategory;
